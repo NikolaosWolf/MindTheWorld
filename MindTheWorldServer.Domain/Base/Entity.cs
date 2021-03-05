445eba97e477798e86866b9b3d87e3b2f694a5aa
Nikolaos Tselikas - Kourtidis <nikolaos.wolf@hotmail.com>
2021-03-05 22:40:32 +0200

    Introduced Entity base model.

---
+++ MindTheWorldServer.Domain/Base/Entity.cs
@@ -0,0 +1,53 @@
+﻿namespace MindTheWorldServer.Domain.Base
+{
+    /// <summary>
+    /// This is the Base Entity Object. All entities must inherit from this.
+    /// </summary>
+    public abstract class Entity<TKey> : IEntity<TKey>
+    {
+        /// <summary>
+        /// Entity Identifier
+        /// </summary>
+        public virtual TKey Id { get; set; }
+
+        /// <inheritdoc cref="IEntity{TId}.IsTransient"/>
+        public abstract bool IsTransient { get; }
+
+        /// <summary>
+        /// Equality comparer.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj.GetType() != GetType())
+                return false;
+
+            return ReferenceEquals(obj, this) || Equals((IEntity<TKey>)obj);
+        }
+
+        /// <summary>
+        /// Equality comparer
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        protected virtual bool Equals(IEntity<TKey> entity)
+        {
+            return !IsTransient
+                && !entity.IsTransient
+                && Id.Equals(entity.Id);
+        }
+
+        /// <summary>
+        /// Override of GetHashCode()
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return 2108858624 + Id.GetHashCode();
+        }
+    }
+}
