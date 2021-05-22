52ed0eeb93f0c58e1726a6380de72cb1af4ad30d
Nikolaos Tselikas - Kourtidis <nikolaos.wolf@hotmail.com>
2021-05-22 11:39:37 +0300

    Minor changes to indexes

--- MindTheWorldServer.Database/Tables/HappinessScore.sql
+++ MindTheWorldServer.Database/Tables/HappinessScore.sql
@@ -4,6 +4,6 @@
     [Year] INT NOT NULL, 
     [Value] DECIMAL(10, 4) NULL, 
 
-    CONSTRAINT PK_ID PRIMARY KEY ([CountryId], [Year]),
+    CONSTRAINT PK_HS_ID PRIMARY KEY ([CountryId], [Year]),
     CONSTRAINT FK_CountryId FOREIGN KEY (CountryId) REFERENCES Country(Id)
 )
