625a707a88e2c115e0481a4b3a6bcb1e1338abe6
Nikolaos Tselikas - Kourtidis <nikolaos.wolf@hotmail.com>
2021-05-26 00:45:36 +0300

    Minor type fixes on value types

--- MindTheWorldServer.Database/Tables/OilConsumption.sql
+++ MindTheWorldServer.Database/Tables/OilConsumption.sql
@@ -2,7 +2,7 @@
 (
 	[CountryId] INT NOT NULL , 
     [Year] INT NOT NULL, 
-    [Value] BIGINT NULL, 
+    [Value] DECIMAL(18, 6) NULL, 
 
     CONSTRAINT PK_OC_ID PRIMARY KEY ([CountryId], [Year]),
     CONSTRAINT FK_OC_CountryId FOREIGN KEY (CountryId) REFERENCES Country(Id)
--- MindTheWorldServer.Database/Tables/WaterWithdrawlPerPerson.sql
+++ MindTheWorldServer.Database/Tables/WaterWithdrawlPerPerson.sql
@@ -2,7 +2,7 @@
 (
 	[CountryId] INT NOT NULL , 
     [Year] INT NOT NULL, 
-    [Value] DECIMAL(10, 6) NULL, 
+    [Value] DECIMAL(18, 6) NULL, 
 
     CONSTRAINT PK_WWPP_ID PRIMARY KEY ([CountryId], [Year]),
     CONSTRAINT FK_WWPP_CountryId FOREIGN KEY (CountryId) REFERENCES Country(Id)
